ed8561363df6fc9066b078abb7a47445f0b71562
Boris <lopez98boris@gmail.com>
2021-03-21 11:59:53 -0400

    Avanzando pantalla de informacion del pedido

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,17 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import {MatStepperModule} from '@angular/material/stepper';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatDividerModule} from '@angular/material/divider';
+
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatButtonModule,
-    MatStepperModule
+    MatStepperModule,
+    MatExpansionModule,
+    MatDividerModule
 
   ],exports:[
     MatButtonModule,
-    MatStepperModule
+    MatStepperModule,
+    MatExpansionModule,
+    MatDividerModule
   ]
 })
 export class MaterialModule { }
