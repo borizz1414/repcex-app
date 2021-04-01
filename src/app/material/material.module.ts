bdbc83315cefc2dcc7a60051726f78d3517e7fbe
Boris <lopez98boris@gmail.com>
2021-03-31 21:51:17 -0400

    Desplegable chips mis pedidos

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -14,6 +14,7 @@ import {MatTableModule} from '@angular/material/table';
 import {MatSortModule} from '@angular/material/sort';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import {MatSelectModule} from '@angular/material/select';
+import {MatMenuModule} from '@angular/material/menu';
 
 @NgModule({
   declarations: [],
@@ -32,7 +33,8 @@ import {MatSelectModule} from '@angular/material/select';
     MatTableModule,
     MatSortModule,
     MatSlideToggleModule,
-    MatSelectModule
+    MatSelectModule,
+    MatMenuModule
 
   ],exports:[
     MatButtonModule,
@@ -48,7 +50,8 @@ import {MatSelectModule} from '@angular/material/select';
     MatTableModule,
     MatSortModule,
     MatSlideToggleModule,
-    MatSelectModule
+    MatSelectModule,
+    MatMenuModule
   ]
 })
 export class MaterialModule { }
